5e4232d94ea1af39131e3e0b3de92899ba38eda8
Yuneste <yunestibi.20.yt@gmail.com>
2023-12-03 13:26:28 +0100

    pc commit

--- complete 2023 webdev bootcamp/Section 11-18 JS and DOM/Drum Kit Starting Files/index.js
+++ complete 2023 webdev bootcamp/Section 11-18 JS and DOM/Drum Kit Starting Files/index.js
@@ -1,3 +1,45 @@
+for(var buttonCount = document.querySelectorAll(".drum").length, i = 0; i < buttonCount; i++)
+document.querySelectorAll("button")[i].addEventListener("click",function() {
+    this.innerHTML,makeSound(this.innerHTML),buttonAnimation(this.innerHTML)
+});
+
+function makeSound(n) {
+    switch(n){
+        case"w":
+        new Audio("sounds/tom-1.mp3").play();
+        break;
+        case"a":
+        new Audio("sounds/tom-2.mp3").play();
+        break;
+        case"s":
+        new Audio("sounds/tom-3.mp3").play();
+        break;
+        case"d":
+        new Audio("sounds/tom-4.mp3").play();
+        break;
+        case"j":
+        new Audio("sounds/snare.mp3").play();
+        break;
+        case"k":
+        new Audio("sounds/crash.mp3").play();
+        break;
+        case"l":
+        new Audio("sounds/kick-bass.mp3").play();
+        break;
+        default:
+            console.log(buttonInnerHTML)}
+        }
+
+function buttonAnimation(n) {
+    var e = document.querySelector("." + n);
+    e.classList.add("pressed"), setTimeout(function() {
+        e.classList.remove("pressed")
+    }, 100)
+}
+
+addEventListener("keydown",function(n){makeSound(n.key),buttonAnimation(n.key)});
+
+/*
 var buttonCount =  document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < buttonCount; i++) {
@@ -5,7 +47,7 @@ for(var i = 0; i < buttonCount; i++) {
         var buttonInnerHTML = this.innerHTML;
         makeSound(this.innerHTML);
         buttonAnimation(this.innerHTML);
-    });
+    }); 
 }
 
 addEventListener("keydown", function (event) {
@@ -52,3 +94,4 @@ function buttonAnimation(currentKey) {
     activeButton.classList.add("pressed");
     setTimeout(function() {activeButton.classList.remove("pressed");}, 100);
 }
+*/
--- complete 2023 webdev bootcamp/Section 19 - jQuery/Tutorial_1/index.js
+++ complete 2023 webdev bootcamp/Section 19 - jQuery/Tutorial_1/index.js
@@ -1,1 +1,72 @@
-$("h1").css("color", "red");
+//$("h1").addClass("big-title margin-50");
+
+
+// document.querySelector("button").innerHTML = "Don't click me";
+// $("button").html("<em>Click me</em>"); /* if you replace html with text then it will also include the tags */
+// $("button").eq(0).text("Don't click me!");
+// $("button:first").text("Don't click me!");
+// $("button").slice(1, 4).text("Don't click me");
+
+
+// console.log($("img").attr("src"));
+// $("a").attr("href", "https://www.yahoo.com");
+
+
+/*
+$("h1").click(function() {
+    $("h1").css("color", "purple")
+});
+*/
+
+
+/*
+$("button").click(function() {
+    $("h1").css("color", "purple");
+});
+*/
+
+
+/*
+$("input").keypress(function(event) {
+    console.log(event.key);
+});
+*/
+
+
+/*
+$("input").keypress(function(event) {
+    $("h1").text(event.key);
+});
+*/
+
+
+/*
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "purple");
+});
+*/
+
+
+/*
+ $("button").on("click" ,function() {
+  //$("h1").toggle(100);  // toggle can also be left empty
+  //$("h1").fadeOut();
+  //$("h1").fadeToggle();
+  //$("h1").slideUp();
+  //$("h1").slideDown();
+  $("h1").slideToggle();
+ });
+ */
+
+
+/*
+ $("button").on("click" ,function() {
+    $("h1").animate({margin: "20%"}); // this must be a numerical value
+ });
+ */
+
+
+
+ $("button").on("click" ,function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+ });
